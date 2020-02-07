8afd7e97ef5ab6599b488c2e7b04fa561d5e4f73
Rimson Liu <rimson@qq.com>
2020-02-07 17:57:29 +0800

    [feat] add board GridView

--- lib/widget/BoardDialog.dart
+++ lib/widget/BoardDialog.dart
@@ -2,9 +2,25 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:unique_bbs/config/resource.dart';
 import 'package:unique_bbs/config/route.dart';
 
 class BoardDialog extends ModalRoute<void> {
+  List<String> _boards = [
+    'report',
+    '通知\n公告',
+    '项目\n任务',
+    '新人\n任务',
+    '文件\n存留',
+    '知识\n分享',
+    '闲谈\n杂论',
+    '奇思\n妙想',
+    '联创\n趣梗',
+  ];
+
+  TextStyle boardTextStyle =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+
   @override
   Color get barrierColor => Colors.black.withOpacity(0.5);
 
@@ -35,10 +51,7 @@ class BoardDialog extends ModalRoute<void> {
   Widget _buildBoardDialogContent(BuildContext context) {
     return Center(
       child: BackdropFilter(
-        filter: ImageFilter.blur(
-          sigmaX: 5.0,
-          sigmaY: 5.0
-        ),
+        filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
@@ -46,20 +59,48 @@ class BoardDialog extends ModalRoute<void> {
               '请先选择发帖板块',
               style: TextStyle(color: Colors.white, fontSize: 30.0),
             ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, BBSRoute.posting);
-              },
-              child: Text('我是板块'),
+            Flexible(child: _buildBoardGridView()),
+            SizedBox(
+              width: 50,
+              height: 50,
+              child: RaisedButton(
+                  shape: CircleBorder(),
+                  padding: EdgeInsets.all(0.0),
+                  child: Icon(
+                    Icons.close,
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                  color: Theme.of(context).indicatorColor,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
             ),
-            RaisedButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text('关闭'),
-            )
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildBoardGridView() {
+    return GridView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, childAspectRatio: 1.0),
+        itemCount: _boards.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.all(25),
+            child: RaisedButton(
+                child: Text(_boards[index], style: boardTextStyle),
+                shape: CircleBorder(),
+                color: ColorResource.backgroundWhite,
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, BBSRoute.posting);
+                }),
+          );
+        });
   }
 }
