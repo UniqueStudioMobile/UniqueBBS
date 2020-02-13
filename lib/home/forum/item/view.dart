cca611ed62204559f66d34a279be3ebb96ca7663
dizzylay <liaoyanglay@outlook.com>
2020-02-13 14:03:08 +0800

    [fix] forum list view ui

--- lib/home/forum/item/view.dart
+++ lib/home/forum/item/view.dart
@@ -1,24 +1,29 @@
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
+import 'package:unique_bbs/config/resource.dart';
 
 import 'action.dart';
 import 'state.dart';
 
 Widget buildView(ItemState state, Dispatch dispatch, ViewService viewService) {
   return Container(
-    margin: EdgeInsets.symmetric(vertical: 18, horizontal: 20),
-    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+    margin: EdgeInsets.only(left: 20, right: 20, bottom: 34),
+    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
     decoration: BoxDecoration(
-      color: Color(0xffffffff),
+      color: ColorResource.backgroundWhite,
       borderRadius: BorderRadius.circular(16),
     ),
     height: 125,
     child: GestureDetector(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
           Row(
             children: <Widget>[
-              FlutterLogo(size: 18),
+              Container(
+                margin: EdgeInsets.only(right: 4),
+                child: FlutterLogo(size: 18),
+              ),
               Expanded(
                 flex: 1,
                 child: Text(
@@ -32,7 +37,8 @@ Widget buildView(ItemState state, Dispatch dispatch, ViewService viewService) {
               ),
               Icon(
                 Icons.chevron_right,
-                size: 16,
+                size: 20,
+                color: Color(0xff9e9e9e),
               )
             ],
           ),
@@ -44,29 +50,47 @@ Widget buildView(ItemState state, Dispatch dispatch, ViewService viewService) {
                     'http://thecatapi.com/api/images/get?format=src'
                     '&size=small&type=jpg#${state.hashCode}'),
               ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "2020年举办全球Hackday吧！",
-                    style: TextStyle(
-                      color: Color(0xff000000),
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
+              Container(
+                margin: EdgeInsets.only(left: 8),
+                height: 48,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Text(
+                          "2020年举办全球Hackday吧",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.fade,
+                          maxLines: 1,
+                        ),
+                        Text(
+                          " HOT",
+                          style: TextStyle(
+                            color: ColorResource.primaryColor,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ],
                     ),
-                    overflow: TextOverflow.fade,
-                    maxLines: 1,
-                  ),
-                  Text(
-                    "2020.01.19 肖宇轩 发布",
-                    style: TextStyle(
-                      color: Color(0xff999999),
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
+                    Text(
+                      "2020.01.19 肖宇轩 发布",
+                      style: TextStyle(
+                        color: ColorResource.textGray,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ],
           ),
